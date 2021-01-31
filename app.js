864cd08348c2f35d160960b469910d9ff0ee0cb5
arahal81 <arahal81@gmail.com>
2021-01-31 15:02:54 +0200

    add css file and updet app.JS

--- app.js
+++ app.js
@@ -1,29 +1,43 @@
-alert('Welcom to Git Slim With Sport Website');
-var nickName = prompt('Please enter your nickName here');
-while (nickName == '') {
-    nickName = prompt('the nick name is empty Please enter your nickName again');
+
+var wlcMessage = function () {
+
+    alert('Welcom to Git Slim With Sport Website');
+}
+function showImges() {
+    var nickName = prompt('Please enter your nickName here');
+    while (nickName == '') {
+        nickName = prompt('the nick name is empty Please enter your nickName again');
+    }
+
+    var fav = prompt('Hi ' + nickName + ', pls enter your favorit spot football or tennis or basketball');
+    while (fav !== 'football' && fav !== 'tennis' && fav !== 'basketball') {
+        fav = prompt('please write only football or tennis or basketball !');
+    }
+    document.writeln('Your name: ' + nickName + '.' + '<br>');
+
+
+    console.log(wlcMessage)
+    var img = '';
+    if (fav === "basketball") {
+        img = '<img src="uplods/basketball1.jpg" />';
+    }
+    else if (fav === "football") {
+        img = '<img src="uplods/football.jpg" />';
+    }
+    else {
+        img = '<img src="uplods/tennis.jpg" />';
+    }
+    var imgCount = prompt('enter numper of images you needed!')
+    var sumImg = '';
+    for (var i = 0; i < imgCount; i++) {
+
+        sumImg += img;
+
+    }
+    document.writeln(sumImg);
 }
 
-var fav = prompt('Hi ' + nickName + ', pls enter your favorit spot football or tennis or basketball');
-while (fav !== 'football' && fav !== 'tennis' && fav !== 'basketball') {
-    fav = prompt('please write only football or tennis or basketball !');
-}
-var img = '';
-if (fav === "basketball") {
-    img = '<img src="uplods/basketball1.jpg" />';
-}
-else if (fav === "football") {
-    img = '<img src="uplods/football.jpg" />';
-}
-else {
-    img = '<img src="uplods/tennis.jpg" />';
-}
-var imgCount = prompt('enter numper of images you needed!')
-var sumImg = '';
-for (var i = 0; i < imgCount; i++) {
 
-    sumImg += img;
 
-}
-document.writeln('Your name: ' + nickName + '.' + '<br>');
-document.writeln(sumImg);
+
+
